486887a21263bce25cd9ec264fa1ddef0bc29ff8
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-10-12 21:20:55 -0500

    working on player character animation

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
+const gravity = 0.5;
+
 class Player {
   constructor(position) {
     this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
   }
 
   draw() {
@@ -16,7 +22,8 @@ class Player {
 
   update() {
     this.draw();
-    this.position.y++;
+    this.position.y += this.velocity.y;
+    this.velocity.y += gravity;
   }
 }
 

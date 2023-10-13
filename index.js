4e88afd2a7f9f06dad42b0aae0f63d8383797dfa
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-10-12 21:14:18 -0500

    started work on player character animation

--- index.js
+++ index.js
@@ -11,19 +11,22 @@ class Player {
       y: 0,
     };
   }
-}
 
-let y = 100;
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, 100, 100);
+  }
+
+  update() {
+    this.position.y++;
+  }
+}
 
 function animate() {
   window.requestAnimationFrame(animate);
 
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = "red";
-  ctx.fillRect(200, y, 100, 100);
-  y++;
 }
 
 animate();

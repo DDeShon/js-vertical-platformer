f369400018dbe1b96bc06c28b19d8d177f87a294
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-10-12 21:24:24 -0500

    working on player character animation

--- index.js
+++ index.js
@@ -13,17 +13,23 @@ class Player {
       x: 0,
       y: 1,
     };
+    this.width = 100;
+    this.height = 100;
   }
 
   draw() {
     ctx.fillStyle = "red";
-    ctx.fillRect(this.position.x, this.position.y, 100, 100);
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
   update() {
     this.draw();
     this.position.y += this.velocity.y;
-    this.velocity.y += gravity;
+    if (this.position.y + this.height + this.velocity.y < canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
   }
 }
 

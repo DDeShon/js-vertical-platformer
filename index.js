36ab43160bff4050c2cc7807e9b46987203463a0
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-10-14 22:26:57 -0500

    working on background image

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ canvas.height = 576;
 const gravity = 0.5;
 
 class Sprite {
-  constructor(position) {
+  constructor({ position, imageSrc }) {
     this.position = position;
+    this.image = new Image();
+    this.image.src = imageSrc;
   }
 
   draw() {
